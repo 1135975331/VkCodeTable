3f2e414a40c08413c305d89813e3542e5f28ce69
1135975331 <78999103+1135975331@users.noreply.github.com>
2022-03-18 01:37:26 +0900

    Add files via upload

---
+++ VkCodeTable.cs
@@ -0,0 +1,182 @@
+﻿namespace Extensions
+{
+	public static class VkCodeTable
+	{
+		public const int LEFT_MOUSE_BUTTON   = 1;
+		public const int RIGHT_MOUSE_BUTTON  = 2;
+		public const int CTRL_BREAK_PROC     = 3;
+		public const int MIDDLE_MOUSE_BUTTON = 4;
+		public const int X1_MOUSE_BUTTON     = 5;
+		public const int X2_MOUSE_BUTTON     = 6;
+
+		public const int BACKSPACE = 8;
+		public const int TAB       = 9;
+		
+		public const int CLEAR = 12;
+		public const int ENTER = 13;
+
+		public const int SHIFT       = 16; //Use L_SHIFT(160) or R_SHIFT(161) instead.
+		public const int CTRL        = 17;  //Use L_CTRL(162)  or R_CTRL(163)  instead.
+		public const int ALT         = 18;   //Use L_ALT(164)   or R_ALT(165)   instead.
+		public const int PAUSE       = 19;
+		public const int CAPS_LOCK   = 20;
+		public const int IME_HANGUEL = 21;  //IME_KANA == IME_HANGUEL == 21
+		
+		public const int IME_JUNJA = 22;
+		public const int IME_FINAL = 23;
+		public const int IME_HANJA = 24;
+		public const int IME_KANJI = 25;
+		
+		public const int ESC            = 27;
+		public const int IME_CONVERT    = 28;
+		public const int IME_NONCONVERT = 29;
+		public const int IME_ACCEPT     = 30;
+		public const int IME_MODE_CHANGE_REQ = 31;
+		
+		public const int SPACE        = 32;
+		public const int PAGE_UP      = 33;
+		public const int PAGE_DOWN    = 34;
+		public const int END          = 35;
+		public const int HOME         = 36;
+		public const int LEFT_ARROW   = 37;
+		public const int UP_ARROW     = 38;
+		public const int RIGHT_ARROW  = 39;
+		public const int DOWN_ARROW   = 40;
+		public const int SELECT       = 41;
+		public const int PRINT        = 42;
+		public const int EXECUTE      = 43;
+		public const int PRINT_SCREEN = 44;
+		public const int INSERT       = 45;
+		public const int DELETE       = 46;
+		public const int HELP         = 47;
+		
+		public const int MAIN_0 = 48;
+		public const int MAIN_1 = 49;
+		public const int MAIN_2 = 50;
+		public const int MAIN_3 = 51;
+		public const int MAIN_4 = 52;
+		public const int MAIN_5 = 53;
+		public const int MAIN_6 = 54;
+		public const int MAIN_7 = 55;
+		public const int MAIN_8 = 56;
+		public const int MAIN_9 = 57;
+		
+		public const int A = 65;
+		public const int B = 66;
+		public const int C = 67;
+		public const int D = 68;
+		public const int E = 69;
+		public const int F = 70;
+		public const int G = 71;
+		public const int H = 72;
+		public const int I = 73;
+		public const int J = 74;
+		public const int K = 75;
+		public const int L = 76;
+		public const int M = 77;
+		public const int N = 78;
+		public const int O = 79;
+		public const int P = 80;
+		public const int Q = 81;
+		public const int R = 82;
+		public const int S = 83;
+		public const int T = 84;
+		public const int U = 85;
+		public const int V = 86;
+		public const int W = 87;
+		public const int X = 88;
+		public const int Y = 89;
+		public const int Z = 90;
+	
+		public const int L_WINDOWS      = 91;
+		public const int R_WINDOWS      = 92;
+		public const int APPLICATIONS   = 93;
+		public const int COMPUTER_SLEEP = 95;
+	
+		public const int NUMPAD_0 = 96;
+		public const int NUMPAD_1 = 97;
+		public const int NUMPAD_2 = 98;
+		public const int NUMPAD_3 = 99;
+		public const int NUMPAD_4 = 100;
+		public const int NUMPAD_5 = 101;
+		public const int NUMPAD_6 = 102;
+		public const int NUMPAD_7 = 103;
+		public const int NUMPAD_8 = 104;
+		public const int NUMPAD_9 = 105;
+
+		public const int NUMPAD_MULTIPLY  = 106;
+		public const int NUMPAD_ADD       = 107;
+		public const int NUMPAD_SEPARATOR = 108;
+		public const int NUMPAD_SUBTRACT  = 109;
+		public const int NUMPAD_DECIMAL   = 110;
+		public const int NUMPAD_DIVIDE    = 111;
+
+		public const int F1  = 112;
+		public const int F2  = 113;
+		public const int F3  = 114;
+		public const int F4  = 115;
+		public const int F5  = 116;
+		public const int F6  = 117;
+		public const int F7  = 118;
+		public const int F8  = 119;
+		public const int F9  = 120;
+		public const int F10 = 121;
+		public const int F11 = 122;
+		public const int F12 = 123;
+		public const int F13 = 124;
+		public const int F14 = 125;
+		public const int F15 = 126;
+		public const int F16 = 127;
+		public const int F17 = 128;
+		public const int F18 = 129;
+		public const int F19 = 130;
+		public const int F20 = 131;
+		public const int F21 = 132;
+		public const int F22 = 133;
+		public const int F23 = 134;
+		public const int F24 = 135;
+
+		public const int NUM_LOCK    = 144;
+		public const int SCROLL_LOCK = 145;
+		
+		public const int L_SHIFT = 160;
+		public const int R_SHIFT = 161;
+		public const int L_CTRL  = 162;
+		public const int R_CTRL  = 163;
+		public const int L_ALT   = 164;
+		public const int R_ALT   = 165;
+		
+		public const int BROWSER_BACK       = 166;
+		public const int BROWSER_FORWARD    = 167;
+		public const int BROWSER_REFRESH    = 168;
+		public const int BROWSER_STOP       = 169;
+		public const int BROWSER_SEARCH     = 170;
+		public const int BROWSER_FAVORITES  = 171;
+		public const int BROWSER_START_HOME = 172;
+		public const int VOLUME_MUTE      = 173;
+		public const int VOLUME_DOWN      = 174;
+		public const int VOLUME_UP        = 175;
+		public const int NEXT_TRACK       = 176;
+		public const int PREV_TRACK       = 177;
+		public const int STOP_MEDIA       = 178;
+		public const int PLAY_PAUSE_MEDIA = 179;
+		public const int START_MAIL       = 180;
+		public const int SELECT_MEDIA     = 181;
+		public const int START_APP_1      = 182;
+		public const int START_APP_2      = 183;
+
+		public const int SEMICOLON = 186;
+		public const int PLUS      = 187;
+		public const int COMMA     = 188;
+		public const int SUBTRACT  = 189;
+		public const int PERIOD    = 190;
+		public const int SLASH     = 191;
+		public const int TILDE     = 192;
+		
+		public const int L_BRACKET = 193;
+		public const int BACKSLASH = 194;
+		public const int R_BRACKET = 195;
+		public const int QUOTE     = 196;
+		
+	}
+}
